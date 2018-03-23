fdf4fe185683a4f614494a47c0fa1ee0f85eddfb
ismael1987 <ismael1987@example.com>
2018-03-24 00:01:09 +0100

    Mp4 the english gentel man

---
+++ src/main/java/com/Mp4/The/English/Gentleman/Mp4/The/English/Gentleman/Mp4TheEnglishGentlemanApplication.java
@@ -0,0 +1,12 @@
+package com.Mp4.The.English.Gentleman.Mp4.The.English.Gentleman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp4TheEnglishGentlemanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp4TheEnglishGentlemanApplication.class, args);
+	}
+}
---
+++ src/test/java/com/Mp4/The/English/Gentleman/Mp4/The/English/Gentleman/Mp4TheEnglishGentlemanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Mp4.The.English.Gentleman.Mp4.The.English.Gentleman;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp4TheEnglishGentlemanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
